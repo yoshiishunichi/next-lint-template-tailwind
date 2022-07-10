6854744d67bb429829feb4de53be3641237bc254
吉井駿一 <syun_syun01@icloud.com>
2022-07-10 10:19:59 +0900

    :wrench: tailwind.config.jsに型補完を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
 module.exports = {
   content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
   plugins: [],
